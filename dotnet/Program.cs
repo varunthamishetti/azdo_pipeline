84e277f5714e426b0fdbdd18aadd436ce7a4da52
Varun Thamishetti <vthamishetti@osidigital.com>
2024-04-26 11:02:14 +0530

    azure devops

---
+++ dotnet/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, .NET Core!");
+        }
+    }
+}
